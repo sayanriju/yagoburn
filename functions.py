c116564ee98dd4ec80ebca38bab28d170d9de5d0
Sayan "Riju" Chakrabarti <sayanriju@users.noreply.github.com>
2009-09-10 14:26:35 +0000

    Handler AddAudioTrack implemented

--- functions.py
+++ functions.py
@@ -1,2 +1,27 @@
 #!/usr/bin/env python
 #coding=utf-8
+
+def FormatSize(size):
+	if size<1024:
+		return "{0:.2f} B".format(size)
+	elif size<1024**2:
+		return "{0:.2f} KB".format(size/(1024.0))
+	elif size<1024**3:
+		return "{0:.2f} MB".format(size/(1024.0**2))		
+	else:
+		return "{0:.2f} GB".format(size/(1024.0**3))
+
+def AddFileDialog(header,filters):
+	import wx
+	application = wx.PySimpleApp()
+	#filters = 'All files (*.*)|*.*|Text files (*.txt)|*.txt'
+	selected=[]
+	
+	dialog = wx.FileDialog ( None, message = header, wildcard = filters, style = wx.OPEN | wx.MULTIPLE )
+	
+	if dialog.ShowModal() == wx.ID_OK:
+	   selected = dialog.GetPaths()
+	dialog.Destroy()
+	
+	return selected
+	
