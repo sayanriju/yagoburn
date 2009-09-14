23f74f58b8af595aed81543c6d7074a49ea1b303
Sayan "Riju" Chakrabarti <sayanriju@users.noreply.github.com>
2009-09-14 11:32:16 +0000

    license,version, readme added

--- functions.py
+++ functions.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 #coding=utf-8
 
+#       This file is part of Yagoburn, which is FREE software; 
+#		you can redistribute it and/or modify it under the terms of 
+#		the GNU General Public License as published by
+#       the Free Software Foundation; either version 3 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA
+#
+#		Copyright 2009-10 Sayan "Riju" Chakrabarti <me@sayanriju.co.cc>
+
+
 def FormatSize(size):
 	if size<1024:
 		return "{0:.2f} B".format(size)
